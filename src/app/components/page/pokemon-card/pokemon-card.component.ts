e959998206056d48564fe5810c890fda036525b1
Chneemann <andre-kempf@mailbox.org>
2024-10-20 12:33:32 +0200

    feat: Implement API data loading and display for Pokémon abilities

--- src/app/components/page/pokemon-card/pokemon-card.component.ts
+++ src/app/components/page/pokemon-card/pokemon-card.component.ts
@@ -14,6 +14,7 @@ export class PokemonCardComponent implements OnInit {
   @Output() cardClosed = new EventEmitter<string>();
 
   pokemonDetails: any[] = [];
+  pokemonAbility: any[] = [];
   openStats: string = 'base-stats';
 
   constructor(private pokemonApiService: PokemonApiService) {}
@@ -27,7 +28,46 @@ export class PokemonCardComponent implements OnInit {
       .getPokemonDetails(this.pokemonName)
       .subscribe((data) => {
         this.pokemonDetails.push(data);
+        this.loadAbilities(data.abilities);
       });
+  }
+
+  loadPokemonAbility(id: number) {
+    this.pokemonApiService.getPokemonAbilities(id).subscribe((data) => {
+      this.pokemonAbility.push(data);
+    });
+  }
+
+  loadAbilities(abilities: any[]) {
+    abilities.forEach((ability) => {
+      const abilityId = this.getIdFromUrl(ability.ability.url);
+      if (abilityId) {
+        this.loadPokemonAbility(+abilityId);
+      }
+    });
+  }
+
+  getIdFromUrl(url: string): string {
+    const regex = /\/(\d+)\//;
+    const match = url.match(regex);
+    return match![1];
+  }
+
+  searchAbilityId(id: number) {
+    return this.pokemonAbility.find((ability) => ability.id === id);
+  }
+
+  getAbilityEffect(currentAbility: any): string {
+    const effectEntries = currentAbility.effect_entries;
+    const englishEntry = effectEntries.find(
+      (entry: any) => entry.language.name === 'en'
+    );
+    if (englishEntry) {
+      return englishEntry.short_effect;
+    }
+    return effectEntries.length > 0
+      ? effectEntries[0].short_effect
+      : 'No description available.';
   }
 
   closePokemonCard() {
--- src/app/services/pokemon-api.service.ts
+++ src/app/services/pokemon-api.service.ts
@@ -6,15 +6,22 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class PokemonApiService {
-  private apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  private apiUrlPokemon = 'https://pokeapi.co/api/v2/pokemon/';
+  private apiUrlAbilities = 'https://pokeapi.co/api/v2/ability/';
 
   constructor(private http: HttpClient) {}
 
   getPokemonList(offset: number, limit: number): Observable<any> {
-    return this.http.get(this.apiUrl + '?offset=' + offset + '&limit=' + limit);
+    return this.http.get(
+      this.apiUrlPokemon + '?offset=' + offset + '&limit=' + limit
+    );
   }
 
   getPokemonDetails(name: string): Observable<any> {
-    return this.http.get(this.apiUrl + name);
+    return this.http.get(this.apiUrlPokemon + name);
+  }
+
+  getPokemonAbilities(id: number): Observable<any> {
+    return this.http.get(this.apiUrlAbilities + id);
   }
 }
