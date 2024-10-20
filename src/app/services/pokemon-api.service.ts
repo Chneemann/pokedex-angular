9c597faac7f34cce2de371361df51b47cb2be332
Chneemann <andre-kempf@mailbox.org>
2024-10-20 12:35:15 +0200

    docs: Implement JSDoc documentation for service API requests

--- src/app/services/pokemon-api.service.ts
+++ src/app/services/pokemon-api.service.ts
@@ -11,16 +11,32 @@ export class PokemonApiService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Retrieves a list of Pokemon based on the provided offset and limit.
+   * @param offset The starting index of the list.
+   * @param limit The maximum number of Pokemon to retrieve.
+   * @returns An Observable containing the list of Pokemon.
+   */
   getPokemonList(offset: number, limit: number): Observable<any> {
     return this.http.get(
       this.apiUrlPokemon + '?offset=' + offset + '&limit=' + limit
     );
   }
 
+  /**
+   * Retrieves the details of a Pokemon given its name.
+   * @param name The name of the Pokemon.
+   * @returns An Observable containing the Pokemon details.
+   */
   getPokemonDetails(name: string): Observable<any> {
     return this.http.get(this.apiUrlPokemon + name);
   }
 
+  /**
+   * Retrieves the details of a Pokemon ability given its id.
+   * @param id The id of the Pokemon ability.
+   * @returns An Observable containing the Pokemon ability details.
+   */
   getPokemonAbilities(id: number): Observable<any> {
     return this.http.get(this.apiUrlAbilities + id);
   }
